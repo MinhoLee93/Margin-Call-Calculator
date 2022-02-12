4d1cb85d8bf16b40a5b9b9de0857868427645601
minhoelee93 <alsgkgk77@gmail.com>
2022-02-12 11:12:11 +0900

    initial commit

---
+++ src/main/java/com/minholee93/margincallcalculator/MarginCallCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.minholee93.margincallcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarginCallCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarginCallCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/minholee93/margincallcalculator/MarginCallCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minholee93.margincallcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarginCallCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
